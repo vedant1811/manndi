80aadb7146e04f5abc574a0a8c6698cf2a5fac10
Vedant Agarwala <vedant.kota@gmail.com>
2014-02-16 16:08:10 +0530

    removed the set: application line 	modified:   config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
 # config/unicorn.rb
 # Set environment to development unless something else is specified
-set :application, "manndi"
 
 env = ENV["RAILS_ENV"] || "development"
 
@@ -9,7 +8,7 @@ worker_processes 4
 
 # listen on both a Unix domain socket and a TCP port, 
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/#{application}.socket", backlog: 64
+listen "/tmp/manndi.socket", backlog: 64
 
 # Preload our app for more speed
 preload_app true
@@ -17,16 +16,16 @@ preload_app true
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-pid "/tmp/unicorn.#{application}.pid"
+pid "/tmp/unicorn.manndi.pid"
 
 # Production specific settings
 if env == "production"
   # Help ensure your application will always spawn in the symlinked
   # "current" directory that Capistrano sets up.
-  working_directory "/home/ubuntu/#{application}/current"
+  working_directory "/home/ubuntu/manndi/current"
 
   # feel free to point this anywhere accessible on the filesystem user 'ubuntu'
-  shared_path = "/home/ubuntu/#{application}/shared"
+  shared_path = "/home/ubuntu/manndi/shared"
 
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
   stdout_path "#{shared_path}/log/unicorn.stdout.log"
@@ -41,7 +40,7 @@ before_fork do |server, worker|
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
-  old_pid = "/tmp/unicorn.#{application}.pid.oldbin"
+  old_pid = "/tmp/unicorn.manndi.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
