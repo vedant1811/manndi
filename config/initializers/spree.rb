ca0b64bec7435d3af35b247e2243aa8504cd6466
Vedant Agarwala <vedant.kota@gmail.com>
2014-02-19 17:27:25 +0530

    Corrected syntax in earlier commit 	modified:   config/initializers/spree.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,8 +9,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
-  set(:allow_ssl_in_development_and_test => false)
-  set(:allow_ssl_in_production => false)
+  config.set(:allow_ssl_in_development_and_test => false)
+  config.set(:allow_ssl_in_production => false)
 end
 # Spree::Config.set(:allow_ssl_in_development_and_test => false)
 Spree.user_class = "Spree::User"
