8666109ae14a202d6bb122c42a563e8d85c5e5b8
Vedant Agarwala <vedant.kota@gmail.com>
2014-03-29 13:39:44 +0530

    Changed names to ubuntu and manndi 	modified:   config/unicorn_init.sh

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -11,11 +11,14 @@
 set -e
 
 # Feel free to change any of the following variables for your app:
+USERNAME=ubuntu
+PROJECTNAME=manndi
+
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/username/apps/projectname/current
+APP_ROOT=/home/$USERNAME/apps/$PROJECTNAME/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=username
+AS_USER=$USERNAME
 set -u
 
 OLD_PIN="$PID.oldbin"
