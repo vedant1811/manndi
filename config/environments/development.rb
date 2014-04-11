9e2e2dc220fe6eb05f51b53829ecb4c116114074
Vedant Agarwala <vedant.kota@gmail.com>
2014-04-11 21:02:47 +0530

    config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,10 @@ Manndi::Application.configure do
   config.action_controller.perform_caching = false
 
   # mailer
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
