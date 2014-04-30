938162ca5a25fec62d945970f75ff3ef7ff3cdfe
Vedant Agarwala <vedant.kota@gmail.com>
2014-05-01 02:14:36 +0530

    First override 	new file:   app/overrides/spree_fancy/home/change_welcome_message.rb

---
+++ app/overrides/spree_fancy/home/change_welcome_message.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => 'spree/home/index',
+                      :name => 'change_welcome_message',
+                      :replace_contents => "h1",
+                      :text => "Welcome to your manndi. You can buy anything you want and
+                    we will ship it anywhere in <strong>Kolkata</strong>")
