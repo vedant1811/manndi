3106e117376e7bf1b1576bbd303304dc04ecadc8
Vedant Agarwala <vedant.kota@gmail.com>
2014-05-11 12:08:38 +0530

    correct "check_revision" behavior 	modified:   config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,8 +58,8 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
+    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+      puts "WARNING: HEAD is not the same as origin/#{branch}"
       puts "Run `git push` to sync changes."
       exit # TODO make this a check to exit rather than exit
     end
