5ecf886c136ee740d6a835e80e0538284f809917
Vedant Agarwala <vedant.kota@gmail.com>
2014-06-03 23:38:19 +0530

    changed config.assets.precompile to include += %w( search.js ) 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Manndi::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -91,5 +91,5 @@ Manndi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+#   config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 end
