9680acdc97209af095f662053ff81347766d3c9e
Vedant Agarwala <vedant.kota@gmail.com>
2014-06-04 22:29:12 +0530

    added the 'original' tag. and added "within one day" 	modified:   app/overrides/spree_fancy/home/change_welcome_message.rb

--- app/overrides/spree_fancy/home/change_welcome_message.rb
+++ app/overrides/spree_fancy/home/change_welcome_message.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => 'spree/home/index',
                       :name => 'change_welcome_message',
+                      :original => '4489ae7e6eb53d9cfe4c1712aefc4ec72967045a' ,
                       :replace_contents => "h1",
                       :text => "Welcome to your manndi. You can buy anything you want and
-                    we will ship it anywhere in <strong>Kolkata</strong>")
+                    we will ship it anywhere in <strong>Kolkata</strong> within one day")
