f92b04d58c601d2a0b30e8c97888f1c602ec9844
Vedant Agarwala <vedant.kota@gmail.com>
2014-06-05 17:19:21 +0530

    Removed mailer config 	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,17 +11,17 @@ Manndi::Application.configure do
   config.eager_load = true
 
   #mailer
-  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :tls => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => :login,
-    :user_name => "[username]",
-    :password => "[password]"
-  }
+#   config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#     :tls => true,
+#     :address => "smtp.gmail.com",
+#     :port => 587,
+#     :domain => "gmail.com",
+#     :authentication => :login,
+#     :user_name => "[username]",
+#     :password => "[password]"
+#   }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
