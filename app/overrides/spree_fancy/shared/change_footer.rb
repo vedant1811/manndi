f3e3e5203e414c6a8dfaeaebb7fb5df1c16a008b
Vedant Agarwala <vedant.kota@gmail.com>
2014-06-06 18:26:24 +0530

    Added the original tag 	modified:   app/overrides/spree_fancy/shared/change_footer.rb

--- app/overrides/spree_fancy/shared/change_footer.rb
+++ app/overrides/spree_fancy/shared/change_footer.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_footer',
                      :name => 'change_footer',
+                     :original => '9e780e54800dfd750c97050ef8e77e8f43a86f50',
                      :replace => "footer",
                      :partial => "shared/footer")
